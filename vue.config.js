c28466eb807927ccd7a97444fb06f2a8ae725139
Samuel Pedroza <samuelmpedroza@gmail.com>
2024-08-16 16:46:31 -0300

    fix: corrige caminho padrão para uso com domínio personalizado

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === "production" ? "/b13-championship-front/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 };
